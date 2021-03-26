52fa0463746876b90072a07478045929d1b117d3
mauriziogiuffredi91 <giuffro91@gmail.com>
2021-03-26 15:26:52 +0100

    commenti

--- js/tool.js
+++ js/tool.js
@@ -5,5 +5,9 @@ rendere interattiva la pagina usando javascript
 */
 
 /*
-1 
+1 Possibilità di mettere il proprio nome e alert se non inserito
+2 Selezionare i punti e obbligare alla selezione
+3 Calcolo del prezzo sulla base della selezione
+4 Possibilità di calcolare uno sconto sulla base dei coupon (presi da un array)
 */
+
